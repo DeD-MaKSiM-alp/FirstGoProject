6e7df3a6d8101797d7a627828b37864ff349c034
DeDMaKSiM1 <ded.maksim.incorporated@gmail.com>
2025-03-30 12:54:35 +0300

    2 pages has been create
    Contacts page and home page

--- www/main.go
+++ www/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	HandleRequest()
+}
+func HandleRequest() {
+	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/contacts/", ContactsPage)
+	http.ListenAndServe(":8080", nil)
+}
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+func ContactsPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contacts Page")
 }
